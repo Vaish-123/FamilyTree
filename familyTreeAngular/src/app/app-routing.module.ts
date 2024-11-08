03eb98e8cbd1f7ea0a061924ed50caaf0d22ecb5
Vaish-123 <vaishnavnampoothiri@gamil.com>
2024-11-08 12:06:15 +0530

    Core changes, new packages, updates in angular.json

--- familyTreeAngular/src/app/app-routing.module.ts
+++ familyTreeAngular/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './core/guards/auth-guard.service';
 
 const routes: Routes = [
   {
     path: 'main',
     loadChildren: () => import('./features/main/main.module').then(m => m.MainModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule),
+    canMatch: [AuthGuardService]
   },
   {
     path: '', redirectTo: '/main', pathMatch: 'full' // Redirect to main on app load
--- familyTreeAngular/src/app/app.component.ts
+++ familyTreeAngular/src/app/app.component.ts
@@ -13,5 +13,6 @@ export class AppComponent {
 
   constructor(private _sharedService: SharedService) {
     this._sharedService.backGroundImage$.subscribe(imageUrl => this.backgroundImage = imageUrl);
+    // if (!this.backgroundImage) this.backgroundImage = "url('/assets/images/background/pexels-pixabay-531880.jpg')"  //Default background
   }
 }
